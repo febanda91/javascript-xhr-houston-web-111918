6f3cadb57b56241a7ef4da6bbfc915c51a2579c8
febanda01 <febanda91@gmail.com>
2019-01-13 22:21:37 -0600

    done

---
+++ index.js
@@ -0,0 +1,25 @@
+import { runInThisContext } from "vm";
+
+function showRepositories(event, data) {
+    var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r => "<li>" + r.name + "</li>")
+    .join("")}</ul>`;
+
+    document.getElementById("repositories").innerHTML = repoList;
+    }
+
+    showRepositories()
+
+
+
+
+
+function getRepositories() {
+    const req = new XMLHttpRequest();
+    req.open("GET", "https://api.github.com/users/octocat/repos");
+    req.send();
+  }
+
+  getRepositories()
